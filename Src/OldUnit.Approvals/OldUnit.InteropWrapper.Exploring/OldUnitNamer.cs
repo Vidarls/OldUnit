6051d662f4dcaad286d12976a5ff1b71a302b56d
Vidarls <vidar.l.somme@gmail.com>
2011-07-31 19:24:38 +0200

    Started working on a wrapper for ApprovalTests
    http://approvaltests.sourceforge.net

---
+++ Src/OldUnit.Approvals/OldUnit.InteropWrapper.Exploring/OldUnitNamer.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OldUnit.InteropWrapper.Exploring
+{
+	public class OldUnitNamer : ApprovalTests.Core.IApprovalNamer
+	{
+	    public OldUnitNamer(string name, string sourcePath)
+	    {
+	        Name = name;
+	        SourcePath = sourcePath;
+	    }
+
+	    public string Name{get; private set; }
+
+        public string SourcePath {get; private set; }
+
+    }
+
+
+}
---
+++ Src/OldUnit.Approvals/OldUnit.InteropWrapper.Exploring/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ApprovalTests;
+
+namespace OldUnit.InteropWrapper.Exploring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var v = "valuie";
+            var x = "xalue;";
+            Console.WriteLine("Starting");
+            Approvals.DefaultNamerSource = (() => new OldUnitNamer(v, x));
+            Approvals.DefaultReporterSource = (() => new ApprovalTests.Reporters.FileLauncherReporter());
+            x = "changed";
+            Approvals.Approve("TEST");
+            
+            Console.ReadLine();
+        }
+    }
+}
